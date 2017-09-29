c91bdd40ec8df40db759e271df953c451dc54122
TheMushrr00m <funji_2302@outlook.es>
2017-09-29 04:40:02 -0500

    Comments added

--- server/index.js
+++ server/index.js
@@ -1,22 +1,43 @@
+// Referenciamos las librerías a utilizar
+
+// Para manejar los WebSockets utilizaremos la librería 'ws'
+// que nos facilita bastante el trabajo con estos.
 const WS = require("ws").Server
+// Puerto a donde escuchará nuestro servidor WS
 const port = 5001
+// Creamos nuestro servidor de WS
 const server = new WS({ port })
 
+// Nos suscribimos al evento de conexión
+// el cual es llamado cuando un cliente se conecta
 server.on("connection", ws => {
-  console.log("=> Server started...")
+  // El método callback es llamado cuando
+  // se conecta un nuevo cliente y en el argumento "ws"
+  // vamos a tener un "enlace" a este cliente
+
+  // Podemos enviar un mensaje al cliente
+  // de bienvenida apenas se conecte
+  // los parámetros son el nombre del mensaje y un json con los datos
+  //ws.send('welcome', { greeting: 'Welcome WS client!' })
+
+  // Los WebSockets se comunican en base a mensajes
+  // por lo que debemos suscribirnos a cada uno de los
+  // posibles mensajes que los clientes puedan enviar
   ws.on("message", message => {
     console.log(`Received: ${message}`)
-
     message = JSON.parse(message)
-
+    // Ya que el cliente puede enviar mensajes de diferentes tipos
+    // Validamos que el mensaje recibido sea del tipo "name"
     if(message.type === "name") {
       // Store the username that send the email
       ws.userName = message.data
       return
     }
 
-    // Send message to all clients
     for(let client of server.clients) {
+      // Cómo está será una aplicación de chat
+      // enviamos el mensaje a todos los clientes
+      // evitando enviarlo a "nosotros"
       if(client !== ws)
         client.send(JSON.stringify({
           type: "message",
@@ -24,13 +45,17 @@ server.on("connection", ws => {
           data: message.data
         }))
     }
-    // To send a message to an specific client
-    //ws.send(`From server: ${message}`)
   })
 
-  ws.on("close", () => {
+  // Nos suscribirnos también al evento
+  // que se ejcuta cuando un cliente decide terminar la conexión
+  ws.on("close", event => {
     console.log("A client was disconnected...")
+    // Podríamos emitir un mensaje a los demás clientes
+    // indicando la desconexión de uno.
   })
 
+  // Todos los llamados a console.log son del servidor
+  // con própositos de depuración
   console.log("New client connected...")
 })
--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
+// URL dónde se encuentra ejecutando nuestro servidor de WS
 const wsUri = "ws://localhost:5001"
+// Creamos un nuevo "cliente" de WS
 let socket = new WebSocket(wsUri)
-const name = prompt("What is your name?")
-
+// Ya que la aplicación de ejemplo es una sala de chat
+// al iniciar nos pide un nombre de usuario para utilizarlo
+const name = prompt("What is your username?")
 document.getElementById('username-label').innerText = `You are: ${name}`
 
+// Controla la conexión al WS
 socket.onopen = event => {
+  // Descomentar para ver que recibimos por parte del WS
+  //console.log(event)
   socket.send(JSON.stringify({
     type: "name",
     data: name
@@ -14,8 +20,12 @@ socket.onopen = event => {
 
 let messages = document.getElementById("messages")
 
+// Nos suscribimos a los eventos de transferencia de mensajes
+// por parte del servidor, si tuvieramos mensajes de diferentes tipos
+// podríamos llevar a cabo una validación
 socket.onmessage = event => {
-  console.log(event)
+  // Descomentar para ver que recibimos por parte del WS
+  //console.log(event)
   const data = JSON.parse(event.data)
   messages.innerHTML += `<span class="text-success">${data.name}:</\span> ${data.data}<br /\>`
 }
